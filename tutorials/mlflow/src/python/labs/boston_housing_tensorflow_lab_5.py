e08ddf59f692e93a422f51a74271b3a8e469b56b
Jules Damji <dmatrix@comcast.net>
2019-08-26 15:02:31 -0700

    modified to be runnable via different experimental parameters

--- tutorials/mlflow/src/python/labs/boston_housing_tensorflow_lab_5.py
+++ tutorials/mlflow/src/python/labs/boston_housing_tensorflow_lab_5.py
@@ -23,42 +23,60 @@ def main(argv):
     feat_cols = [tf.feature_column.numeric_column(key="features", shape=(x_train.shape[1],))]
     feat_spec = {
         "features": tf.placeholder("float", name="features", shape=[None, x_train.shape[1]])}
-    hidden_units = [50, 20]
+    hidden_units = [1024,256,64,16]
     steps = 1000
-    regressor = tf.estimator.DNNRegressor(hidden_units=hidden_units, feature_columns=feat_cols)
-    train_input_fn = tf.estimator.inputs.numpy_input_fn({"features": x_train}, y_train,
-                                                        num_epochs=None, shuffle=True)
-    with mlflow.start_run():
-        mlflow.log_param("Hidden Units", hidden_units)
-        mlflow.log_param("Steps", steps)
-        regressor.train(train_input_fn, steps=steps)
-        test_input_fn = tf.estimator.inputs.numpy_input_fn({"features": x_test}, y_test,
-                                                           num_epochs=None, shuffle=True)
-        # Compute mean squared error
-        mse = regressor.evaluate(test_input_fn, steps=steps)
-        mlflow.log_metric("Mean Square Error", mse['average_loss'])
-        # Building a receiver function for exporting
-        receiver_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(feat_spec)
-        temp = tempfile.mkdtemp()
-        try:
-            saved_estimator_path = regressor.export_savedmodel(temp, receiver_fn).decode("utf-8")
-            # Logging the saved model
-            mlflow.tensorflow.log_model(tf_saved_model_dir=saved_estimator_path,
-                                        tf_meta_graph_tags=[tag_constants.SERVING],
-                                        tf_signature_def_key="predict",
-                                        artifact_path="model")
-            # Reloading the model
-            pyfunc_model = pyfunc.load_pyfunc(mlflow.get_artifact_uri('model'))
-            df = pd.DataFrame(data=x_test, columns=["features"] * x_train.shape[1])
-            # Predicting on the loaded Python Function
-            predict_df = pyfunc_model.predict(df)
-            predict_df['original_labels'] = y_test
-            print(predict_df)
-        finally:
-            shutil.rmtree(temp)
+    nepochs = [10, 15, 20, 25]
+    for run_n in [0, 1, 2, 3]:
+        regressor = tf.estimator.DNNRegressor(hidden_units=hidden_units, feature_columns=feat_cols)
+        train_input_fn = tf.estimator.inputs.numpy_input_fn({"features": x_train}, y_train,
+                                                         num_epochs=nepochs[run_n], shuffle=True)
+        with mlflow.start_run() as run :
+            runID = run.info.run_uuid
+            experimentID = run.info.experiment_id
+
+            # log parameters
+            mlflow.log_param("Hidden Units", hidden_units)
+            mlflow.log_param("Steps", steps)
+            mlflow.log_param("epochs", nepochs[run_n])
+            regressor.train(train_input_fn, steps=steps)
+            test_input_fn = tf.estimator.inputs.numpy_input_fn({"features": x_test}, y_test,
+                                                       num_epochs=nepochs[run_n], shuffle=True)
+            # Compute mean squared error
+            mse = regressor.evaluate(test_input_fn, steps=steps)
+            mlflow.log_metric("Average Mean Square Error", mse['average_loss'])
+            mlflow.log_metric("Mean Square Error", mse['loss'])
+            # Building a receiver function for exporting
+            receiver_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(feat_spec)
+            temp = tempfile.mkdtemp()
+            try:
+                saved_estimator_path = regressor.export_savedmodel(temp, receiver_fn).decode("utf-8")
+                # Logging the saved model
+                mlflow.tensorflow.log_model(tf_saved_model_dir=saved_estimator_path,
+                                            tf_meta_graph_tags=[tag_constants.SERVING],
+                                            tf_signature_def_key="predict",
+                                            artifact_path="model")
+                # Reloading the model
+                pyfunc_model = pyfunc.load_pyfunc(mlflow.get_artifact_uri('model'))
+                df = pd.DataFrame(data=x_test, columns=["features"] * x_train.shape[1])
+                # Predicting on the loaded Python Function
+                predict_df = pyfunc_model.predict(df)
+                predict_df['original_labels'] = y_test
+
+                # print some data
+                print("Inside MLflow Run with run_id {} and experiment_id {}".format(runID, experimentID))
+                print('Mean Squared Error               :',  mse['average_loss'])
+                print('Mean Squared Error/per batch     :',  mse['loss'])
+                print('Evaluate MSE Dictionary          :', mse)
+
+                print("-" * 100)
+                print(predict_df)
+            finally:
+                shutil.rmtree(temp)
 
 
 if __name__ == "__main__":
     # The Estimator periodically generates "INFO" logs; make these logs visible.
-    tf.logging.set_verbosity(tf.logging.INFO)
+    #tf.logging.set_verbosity(tf.logging.INFO)
+    # Enable auto-logging to MLflow to capture TensorBoard metrics.
+    #mlflow.tensorflow.autolog()
     tf.app.run(main=main)
