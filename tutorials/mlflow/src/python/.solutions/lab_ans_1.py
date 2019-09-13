8caf5fac9f8e097d974eb2b2f6ea5294051d3799
Jules Damji <dmatrix@comcast.net>
2019-09-12 18:55:28 -0700

    added secret dir

---
+++ tutorials/mlflow/src/python/.solutions/lab_ans_1.py
@@ -0,0 +1,166 @@
+'''
+
+Problem - part 1: We want to predict the gas consumption in millions of gallons in 48 of the US states
+based on some key features. These features are petrol tax (in cents), per capital income (in US dollars),
+paved highway (in miles), population of people with driving licences
+
+Solution:
+
+Since this is a regression problem where the value is a range of numbers, we can use the
+common Random Forest Algorithm in Scikit-Learn. Most regression models are evaluated with
+three standard evalution metrics: Mean Absolute Error(MAE); Mean Squared Error (MSE); and
+Root Mean Squared Error (RSME), and r2.
+
+This example is borrowed from the source below, modified and modularized for this tutorial
+source: https://stackabuse.com/random-forest-algorithm-with-python-and-scikit-learn/
+
+Aim of this Lab:
+
+1. Understand MLflow Tracking API
+2. How to use the MLflow Tracking API
+3. Use the MLflow API to experiment several Runs
+4. Interpret and observer runs via the MLflow UI
+
+Some Resources:
+https://mlflow.org/docs/latest/python_api/mlflow.html
+https://www.saedsayad.com/decision_tree_reg.htm
+https://towardsdatascience.com/understanding-random-forest-58381e0602d2
+https://stackabuse.com/random-forest-algorithm-with-python-and-scikit-learn/
+https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestRegressor.html
+'''
+
+import os
+import numpy as np
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+from lab_ans_utils import load_data, plot_graphs, get_mlflow_directory_path, print_pandas_dataset
+
+class RFRModel():
+    """
+    General class for Sckit-learn RandomForestRegressor
+    """
+    # class wide variables common to all instances
+    # keep track of cumulative estimators and rsme
+    rsme = []
+    estimators = []
+    r2 = []
+
+    def __init__(self, params={}):
+        """
+        Construtor for the RandomForestRegressor
+        :param params: dictionary to RandomForestRegressor
+        """
+        self.rf = RandomForestRegressor(**params)
+        self.params = params
+
+    def model(self):
+        """
+        Return the model craeted
+        :return: handle or instance of the RandomForestReqgressor
+        """
+        return self.rf
+
+    def mlflow_run(self, df, r_name="Lab-1:RF Petrol Regression Experiment"):
+        """
+        This method trains, computes metrics, and logs all metrics, parameters,
+        and artifacts for the current run
+        :param df: pandas dataFrame
+        :param r_name: Name of the experiment as logged by MLflow
+        :return: MLflow Tuple (ExperimentID, runID)
+        """
+
+        with mlflow.start_run(run_name=r_name) as run:
+            # get all rows and columns but the last column
+            X = dataset.iloc[:, 0:4].values
+            # get all the last columns, which is what we want to predict
+            y = dataset.iloc[:, 4].values
+
+            # create train and test data
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+            # Feature Scaling
+            sc = StandardScaler()
+            X_train = sc.fit_transform(X_train)
+            X_test = sc.transform(X_test)
+
+            # train and predict
+            self.rf.fit(X_train, y_train)
+            y_pred = self.rf.predict(X_test)
+
+            # Log model and params using the MLflow sklearn APIs
+            mlflow.sklearn.log_model(self.rf, "random-forest-reg-model")
+            mlflow.log_params(self.params)
+
+            # compute  metrics
+            mae = metrics.mean_absolute_error(y_test, y_pred)
+            mse = metrics.mean_squared_error(y_test, y_pred)
+            rsme = np.sqrt(mse)
+            r2 = metrics.r2_score(y_test, y_pred)
+
+            # Log metrics
+            mlflow.log_metric("mae", mae)
+            mlflow.log_metric("mse", mse)
+            mlflow.log_metric("rsme", rsme)
+            mlflow.log_metric("r2", r2)
+
+            # update global class instance variable with values
+            self.rsme.append(rsme)
+            self.r2.append(r2)
+            self.estimators.append(params["n_estimators"])
+
+            # plot RSME graph and save as artifacts
+            (fig, ax) = plot_graphs(rfr.estimators, rfr.rsme, "Random Forest Estimators", "Root Mean Square", "Root Mean Square vs Estimators")
+
+            # get current run and experiment id
+            runID = run.info.run_uuid
+            experimentID = run.info.experiment_id
+
+            # create image artifact directory
+            image_dir = get_mlflow_directory_path(experimentID, runID, "images")
+            save_image = os.path.join(image_dir, "rsme_estimators.png")
+            fig.savefig(save_image)
+
+            # plot R2 graph and save as artifacts
+            (fig_2, ax) = plot_graphs(rfr.estimators, rfr.r2, "Random Forest Estimators", "R2", "R2 vs Estimators")
+            # save images
+            save_image_2 = os.path.join(image_dir, "r2_estimators.png")
+            fig_2.savefig(save_image_2)
+
+            # log artifact
+            mlflow.log_artifacts(image_dir, "images")
+
+            # print some data
+            print("-" * 100)
+            print("Inside MLflow Run with run_id {} and experiment_id {}".format(runID, experimentID))
+            print("Estimator trees        :", self.params["n_estimators"])
+            print('Mean Absolute Error    :', mae)
+            print('Mean Squared Error     :', mse)
+            print('Root Mean Squared Error:', rsme)
+            print('R2                     :', r2)
+
+            return (experimentID, runID)
+
+#
+# TODO in Lab/Homework for Some Experimental runs
+#
+    # 1. Consult RandomForestRegressor documentation
+    # 2. Change or add parameters, such as depth of the tree or random_state: 42 etc.
+    # 3. Change or alter the range of runs and increments of n_estimators.
+    # 4. Check in MLfow UI if the metrics are affected
+    # challenge-1: create mean square error and r2 artifacts and save them for each run
+
+if __name__ == '__main__':
+    # load and print dataset
+    dataset = load_data("data/petrol_consumption.csv")
+    print_pandas_dataset(dataset)
+    # iterate over several runs with different parameters
+    for n in range (50, 350, 50):
+        params = {"n_estimators": n, "random_state": 0 }
+        rfr = RFRModel(params)
+        (experimentID, runID) = rfr.mlflow_run(dataset)
+        print("MLflow Run completed with run_id {} and experiment_id {}".format(runID, experimentID))
+        print("-" * 100)
---
+++ tutorials/mlflow/src/python/.solutions/lab_ans_utils.py
@@ -0,0 +1,150 @@
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.utils.multiclass import unique_labels
+from sklearn.metrics import confusion_matrix
+import os
+import numpy as np
+
+def load_data(path):
+    """
+    Read a CSV file from a given path and return a Pandas DataFrame
+    :param path: path to csv file
+    :return: returns Pandas DataFrame
+    """
+
+    df = pd.read_csv(path)
+    return df
+
+def plot_graphs(x_data, y_data, x_label, y_label, title):
+    """
+    Use the Mathplot lib to plot data points provide and respective x-axis and y-axis labels
+    :param x_data: Data for x-axis
+    :param y_data: Data for y-axis
+    :param x_label: Label for x-axis
+    :param y_label: LabEL FOR Y-axis
+    :param title: Title for the plot
+    :return: return a tuple (fig, ax)
+    """
+
+    plt.clf()
+
+    fig, ax = plt.subplots()
+    ax.plot(x_data, y_data)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
+
+    return (fig, ax)
+
+def plot_residual_graphs(predictions, y_test, x_label, y_label, title):
+    """
+    Create residual plot using seaborn plotting library
+    https://seaborn.pydata.org/tutorial/regression.html
+    :param predictions: predictions from the run
+    :param y_test: actual labels
+    :param x_label: name for the x-axis
+    :param y_label: name for the y-axis
+    :param title:  title for the plot
+    :return: tuple of plt, fig, ax
+    """
+
+    fig, ax = plt.subplots()
+
+    sns.residplot(predictions, y_test, lowess=True)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+
+    return (plt, fig, ax)
+
+def get_mlflow_directory_path(*paths, create_dir=True):
+    """
+    Get the current running path where mlruns is created. This is the directory from which
+    the python file containing MLflow code is executed. This method is used for artifacts, such
+    as images, where we want to store plots.
+    :param paths: list of directories below mlfruns, experimentID, mlflow_run_id
+    :param create_dir: detfault is True
+    :return: path to directory.
+    """
+
+    cwd = os.getcwd()
+    dir = os.path.join(cwd, "mlruns", *paths)
+    if create_dir:
+        if not os.path.exists(dir):
+            os.mkdir(dir, mode=0o755)
+    return dir
+
+
+def print_pandas_dataset(d):
+    """
+    Given a Pandas dataFrame show the dimensions sizes
+    :param d: Pandas dataFrame
+    :return: None
+    """
+    print("rows = %d; columns=%d" % (d.shape[0], d.shape[1]))
+    print(d.head())
+
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    Borrowed from the scikit-learn library documentation
+
+    :param y_true: the actual value of y
+    :param y_pred: the predicted valuye of y
+    :param classes: list of label classes to be predicted
+    :param normalize: normalize the data
+    :param title: title of the plot for confusion matrix
+    :param cmap: color of plot
+    :return: returns a tuple of (plt, fig, ax)
+    """
+
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return (plt, fig, ax)
