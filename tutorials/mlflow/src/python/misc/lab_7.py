b49a5bbe90d5ad08c3654533904a5560222d091c
Jules Damji <dmatrix@comcast.net>
2019-09-17 15:58:00 -0700

    added parameters to misc

--- tutorials/mlflow/src/python/misc/lab_7.py
+++ tutorials/mlflow/src/python/misc/lab_7.py
@@ -3,7 +3,10 @@ import mlflow
 Read documentation on https://mlflow.org/docs/latest/python_api/mlflow.html#mlflow.run
 """
 if __name__ == '__main__':
-    params = {'alpha': 0.5,
+    params_1 = {'alpha': 0.5,
               'l1_ratio': 0.01}
-    mlflow.run("git://github.com/mlflow/mlflow-example.git", use_conda=False, parameters=params)
-    mlflow.run("git://github.com/dmatrix/mlflow-example.git", use_conda=False)
+    params_2 = {'epochs': 5}
+
+    # Two runs from GitHub Project
+    mlflow.run("git://github.com/mlflow/mlflow-example.git", use_conda=False, parameters=params_1)
+    mlflow.run("git://github.com/dmatrix/mlflow-example.git", use_conda=False, parameters=params_2)
