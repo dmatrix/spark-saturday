ac0e0263056b3e449c0e864c4fc77df90c1d2d45
Jules Damji <dmatrix@comcast.net>
2019-10-31 18:27:51 -0700

    include using StandScaler for the test data

--- tutorials/mlflow/src/python/labs/load_predict_model_lab_6.py
+++ tutorials/mlflow/src/python/labs/load_predict_model_lab_6.py
@@ -1,6 +1,7 @@
 import mlflow
 import mlflow.sklearn
 import mlflow.pyfunc
+from sklearn.preprocessing import StandardScaler
 from lab_utils import load_data
 
 class MLflowOps():
@@ -39,13 +40,15 @@ if __name__ == '__main__':
     # get all values only from the last columns, which is what we want to predict
     y_test = dataset.iloc[:, 4].values
     print("Observed values {}".format(y_test))
+    sc = StandardScaler()
+    X_test = sc.fit_transform(X_test)
     print("-" * 100)
     #
     # TODO in Lab
     # Add your run_uids from Lab-1 Runs. <cut-and-past in the list>
     # Can you try Lab-1 runs with model `random-forest-class-mode`, our classification model
     # and use its respective run_uids and test data
-    for run_id in ['a070f2b5176144348cb03c4d95cf6b4a']:
+    for run_id in ['0f1511c9208845c3b708c01b5f75ba91']:
         uri = "runs:/" + run_id + "/random-forest-reg-model"
         sk_model = mclnt.get_model("sklearn")(uri)
         print("Using Sckit-Learn Model Prediction:{}".format(type(sk_model)))
--- tutorials/mlflow/src/python/misc/lab_6.py
+++ tutorials/mlflow/src/python/misc/lab_6.py
@@ -3,6 +3,8 @@ import mlflow.sklearn
 import mlflow.pyfunc
 
 from lab_utils import load_data
+from sklearn.preprocessing import StandardScaler
+
 
 class MLflowOps():
     def __init__(self):
@@ -30,8 +32,10 @@ if __name__ == '__main__':
     # get all the last columns, which is what we want to predict
     y_test = dataset.iloc[:, 4].values
     print("Observed values {}".format(y_test))
+    sc = StandardScaler()
+    X_test = sc.fit_transform(X_test)
     # Insert your run_id here in the list
-    for run_id in ['c0c2010c793345dd80d88da5cac6cdcf']:
+    for run_id in ['c3acc1f495d74bae8c8d5f02766d9dd7']:
         uri = "runs:/" + run_id + "/random-forest-reg-model"
         sk_model = mclnt.get_model("sklearn")(uri)
         print("-" * 100)
